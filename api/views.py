8c9a2bd9d0d7b596330be1c0f964b509c19ebf5a
sajibuzzaman <muhammadsajibuzzaman1998@gmail.com>
2021-04-20 02:04:24 +0600

    add permissions in ProductAPIView

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from shop.models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 
 # Create your views here.
 class ProductAPIView(generics.ListAPIView):
+    permission_classes =(permissions.IsAdminUser,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -12,9 +13,11 @@ class CategoryAPIView(generics.ListAPIView):
     serializer_class = CategorySerializer
 
 class ProductAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes =(permissions.IsAdminUser,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductAPINewView(generics.ListCreateAPIView):
+    permission_classes =(permissions.IsAdminUser,)
     queryset = Product.objects.all().order_by('-id')[:1]
     serializer_class = ProductSerializer
--- giftshop/settings.py
+++ giftshop/settings.py
@@ -51,6 +51,12 @@ INSTALLED_APPS = [
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
